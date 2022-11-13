89a6eeeaf239d1325a9aebe50bc3d5dde54e83cb
Ruslan Ivanov <ruslan4432013@gmail.com>
2022-11-13 20:30:34 +0300

    add console logger

---
+++ src/out/console-logger/console-logger.ts
@@ -0,0 +1,30 @@
+import {IStreamLogger} from "core/handlers/stream-logger.interface";
+
+export class ConsoleLogger implements IStreamLogger {
+    private static instance: ConsoleLogger;
+
+    private constructor() {
+    }
+
+    public static get(): ConsoleLogger {
+        if (!ConsoleLogger.instance) {
+            ConsoleLogger.instance = new ConsoleLogger()
+        }
+        return ConsoleLogger.instance
+    }
+
+    end(): void {
+        console.log('Done')
+    }
+
+    error(...args: any[]): void {
+        console.log('Errors:')
+        console.log(...args)
+    }
+
+    log(...args: any[]): void {
+        console.log('Log:')
+        console.log(...args)
+    }
+
+}
